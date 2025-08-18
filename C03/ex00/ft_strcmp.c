e974b17e5b42f42fe2da254a0c0a403ebfbed500
Yuri Augusto Santos Nunes <ysantos-@student.42lisboa.com>
2025-08-18 19:26:36 +0100

    simplyfied strcmp

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -23,14 +23,10 @@ int	ft_strcmp(char *s1, char *s2)
 	i = -1;
 	while (s1[++i] && s2[i])
 	{
-		if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-		if (s1[i] > s2[i])
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 	}
-	if (s1[i] < s2[i])
-		return (s1[i] - s2[i]);
-	if (s1[i] > s2[i])
+	if (s1[i] != s2[i])
 		return (s1[i] - s2[i]);
 	return (0);
 }
